6c3b3d8b13c5bfdf895f7620c8281a127658f458
sandy3781 <mushi_sp@yahoo.com>
2024-06-24 20:55:03 +0000

    learning if conditions

---
+++ ifcond.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def main():
+    hostname1 = "MTG"
+    hostname2 = "mtg"
+    if hostname2 == hostname1.lower():    
+       print("hostname match")
+    else:
+       print("hostname doesnt match") 
+
+main()         
